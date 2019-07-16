62ff5b911a16d74b42877d31619d07aa428e798e
davip <davi.p.m94@gmail.com>
2019-07-16 17:01:22 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
     <main id="app">
       <div className="box item-1" id="item1">
         <h1>React Smooth Scroll Top</h1>
-        <a href="#top">Github Repository <i className="fa fa-github" aria-hidden="true"></i></a>
+        <a
+          href="https://github.com/davi-94/react-smooth-scroll-to-top"
+          target="_blank" rel="noreferrer noopener">
+          Github Repository {' '}
+          <i className="fa fa-github" aria-hidden="true"></i>
+        </a>
       </div>
       <div className="box item-2" id="item2"></div>
       <div className="box item-3" id="item3"></div>

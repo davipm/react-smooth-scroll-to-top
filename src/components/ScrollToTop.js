77d1a235c0edb3abcb41142cb89180b7af5bb2c8
Davi Pereira <davi.p.m94@gmail.com>
2019-10-08 15:38:29 -0300

    add prop types

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,22 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { CSSTransition } from 'react-transition-group';
-import './ScrollToTop.scss';
+import React, { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+import PropTypes from "prop-types";
+import "./ScrollToTop.scss";
 
-export default function ScrollToTop({visibleAt = 300, right = '30px', bottom = '40px', color }) {
+export default function ScrollToTop({
+  color,
+  visibleAt = 300,
+  right = "30px",
+  bottom = "40px"
+}) {
   const [visibleOffset] = useState(visibleAt);
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     window.smoothScroll = () => {
-      let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      let currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
       if (currentScroll > 0) {
         window.requestAnimationFrame(window.smoothScroll);
-        window.scrollTo(0, Math.floor(currentScroll - (currentScroll / 5)))
+        window.scrollTo(0, Math.floor(currentScroll - currentScroll / 5));
       }
     };
-    window.addEventListener('scroll', catchScroll);
-    return () => window.removeEventListener('scroll', catchScroll);
-  });
+    window.addEventListener("scroll", catchScroll);
+    return () => window.removeEventListener("scroll", catchScroll);
+  }, []);
 
   const catchScroll = () => {
     setVisible(window.pageYOffset > visibleOffset);
@@ -58,5 +65,12 @@ export default function ScrollToTop({visibleAt = 300, right = '30px', bottom = '
         </div>
       </CSSTransition>
     </>
-  )
+  );
 }
+
+ScrollToTop.prototype = {
+  visibleAt: PropTypes.number.isRequired,
+  right: PropTypes.string.isRequired,
+  bottom: PropTypes.string.isRequired,
+  color: PropTypes.string
+};
